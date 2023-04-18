73b5fcc3ef830491c0df55321787422ec234b1b5
Phong Vo <phong.d@nimblehq.co>
2023-04-18 11:03:53 +0700

    Comment plugin for KMM

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,10 @@ buildscript {
     }
 }
 
-plugins {
-    id("com.android.library").version("7.2.2").apply(false)
-    kotlin("multiplatform").version("1.7.10").apply(false)
-}
+// plugins {
+//     id("com.android.library").version("7.2.2").apply(false)
+//     kotlin("multiplatform").version("1.7.10").apply(false)
+// }
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
